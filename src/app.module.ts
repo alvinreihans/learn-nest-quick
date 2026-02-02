6a6f1a6d03934bb475543118b9850e3e70bdc018
Alvin Reihansyah Makarim <reihansyah6@gmail.com>
2026-02-02 10:37:42 +0700

    feat: adding database and environment configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ArticleModule } from './article/article.module';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { typeOrmConfig } from './config/database.config';
 @Module({
-  imports: [ArticleModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot(typeOrmConfig()),
+    ArticleModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
