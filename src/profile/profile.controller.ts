a468e9ca1dd3cb510bebb4d73cd4183cd8396577
Alvin Reihansyah Makarim <reihansyah6@gmail.com>
2026-02-12 14:02:36 +0700

    feat(profile): implement upsert logic for user profile
    - Add POST /profile endpoint
    - Implement logic to create new profile if user.id doesn't exist
    - Update existing profile if user.id is already present
    - Link profile to user via foreign key

--- src/profile/profile.controller.ts
+++ src/profile/profile.controller.ts
@@ -1,7 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { Request as ExpressRequest } from 'express';
 import { ProfileService } from './profile.service';
+import { AuthGuard } from 'src/auth/guard/auth.guard';
+import { createOrUpdateProfileDto } from './dto/createOrUpdateProfile.dto';
 
 @Controller('profile')
+@UseGuards(AuthGuard)
 export class ProfileController {
   constructor(private readonly profileService: ProfileService) {}
+
+  @Post()
+  async updateOrCreateProfile(
+    @Request() request: ExpressRequest & { user: { id: string } },
+    @Body() createOrUpdateProfileDto: createOrUpdateProfileDto,
+  ): Promise<{ message: string }> {
+    return await this.profileService.updateOrCreateProfile(
+      request.user.id,
+      createOrUpdateProfileDto,
+    );
+  }
 }
--- src/profile/profile.service.ts
+++ src/profile/profile.service.ts
@@ -1,4 +1,47 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Profile } from './entities/profile.entity';
+import { Repository } from 'typeorm';
+import { User } from 'src/auth/entities/user.entity';
+import { createOrUpdateProfileDto } from './dto/createOrUpdateProfile.dto';
 
 @Injectable()
-export class ProfileService {}
+export class ProfileService {
+  constructor(
+    @InjectRepository(Profile)
+    private profileRepository: Repository<Profile>,
+
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
+
+  async updateOrCreateProfile(
+    userId: string,
+    createOrUpdateProfileDto: createOrUpdateProfileDto,
+  ): Promise<{ message: string }> {
+    const user = await this.userRepository.findOne({
+      where: { id: userId },
+      relations: ['profile'],
+    });
+    if (!user) {
+      throw new NotFoundException();
+    }
+
+    if (user.profile) {
+      // update profile
+      Object.assign(user.profile, createOrUpdateProfileDto);
+      await this.profileRepository.save(user.profile);
+      return { message: 'Berhasil update profile' };
+    } else {
+      // tambah profile
+      const newProfile = this.profileRepository.create(
+        createOrUpdateProfileDto,
+      );
+      newProfile.user = user;
+      await this.profileRepository.save(newProfile);
+      return {
+        message: 'Berhasil membuat profile',
+      };
+    }
+  }
+}
